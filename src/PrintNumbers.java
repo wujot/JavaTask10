6ba65db4d93bdcba1b519630461f21df1203c932
wujot <wojciech.wujot"gmail.com>
2018-01-24 15:14:00 +0100

    Program is printing numbers between 0.0 and 3.0

---
+++ src/PrintNumbers.java
@@ -0,0 +1,12 @@
+public class PrintNumbers {
+
+    public static void main(String[] args) {
+
+        double i = 0;
+
+        while(i < 3.1) {
+            System.out.printf("%1.1f\n", i);
+            i += 0.1;
+        }
+    }
+}
